bee07ab0954d3cd931bb18dd8785f5d07c0a31bf
Jonathan Cruz Rocha Ferreira <jonathanc@take.net>
2020-08-02 21:26:42 -0300

    alter initial and finish script

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,18 @@ import { Item } from './models/Item';
 const CronJob = require('cron').CronJob
 const data: Store = { items: [] };
 const items: Item[] = [
-  { name: 'Holy Dagger', filters: ['Morale'] },
-  { name: 'Stalker', filters: ['Morale'] },
-  { name: 'Thief', filters: ['+10', 'Morale'] },
-  { name: 'Breath', filters: ['+10', 'Morale'] },
-
+  { name: 'Thief', filters: ['+10', 'Morale', 'Sharp Blade'] },
+  { name: 'Breath', filters: ['+10', 'Morale', 'Sharp Blade'] },
+  { name: 'Strength Ring', filters: ['+10', 'Morale', 'Sharp Blade'] },
+  { name: 'Ancient Cape', filters: ['+12', '+10'] },
+  { name: 'Rosa Bracelet', filters: ['+12'] },
+  { name: 'Evil Bracer', filters: ['+10', '+11', '+12'] },
 ]
 
 const init = async () => {
+  console.log('===================== INICIO ===================');
   await PoringWorld.init(items, data)
+  console.log('====================== FIM =====================');
 }
 
 const job = new CronJob(
